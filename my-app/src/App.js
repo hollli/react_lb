23156e7523b1ae8fec1d168e724e3e4f9f355f69
hollli <63290246+hollli@users.noreply.github.com>
2023-04-02 17:16:00 +0300

    lb2// add more pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from "react-bootstrap";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Blog from "./pages/Blog";
+import React from "react";
+import Header from "./Header";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <div> <h1> Ni hao bracho!</h1>
-              <Button variant="primary">Hit me</Button>
+              <div className="btn-group">
+                  <a href="http://localhost:3000/About">
+                      <Button className="button">About</Button>
+                  </a>
+                  <a href="http://localhost:3000/Contacts" >
+                      <Button className="button">Contacts</Button>
+                  </a>
+                  <a href="http://localhost:3000/Blog" >
+                      <Button className="button">Blog</Button>
+                  </a>
+            </div>
           </div>
       </header>
+        <Router>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contacts" element={<Contacts/>}/>
+            <Route path="/blog" element={<Blog/>}/>
+        </Routes>
+    </Router>
     </div>
+
   );
 }
 
